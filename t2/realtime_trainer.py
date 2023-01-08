bd09a1f90c793248edad862455b8faaa8e2eadf5
go13 <dima.blinkov@gmail.com>
2023-01-08 20:56:05 +0000

    ga + transformer

--- t2/realtime_trainer.py
+++ t2/realtime_trainer.py
@@ -2,7 +2,7 @@ import os
 import sys
 from logging import getLogger
 import torch
-
+import numpy as np
 from t2.abstract_trainer import AbstractTrainer
 from t2.utils import join_sai
 
@@ -15,6 +15,7 @@ class RealtimeTrainer(AbstractTrainer):
         super().__init__(modules, env, params)
 
         self.training_queue = []
+        self.learning_queue = []
         self.data_path = None
 
     def learn(self, x1, x2, y, learning_rate):
@@ -22,14 +23,17 @@ class RealtimeTrainer(AbstractTrainer):
         x2 = join_sai(x2)
         y = join_sai(y)
 
+        self.learning_queue.append(learning_rate)
         self.training_queue.append((x1.split(), x2.split(), y.split()))
 
         if len(self.training_queue) == self.params.batch_size:
             (x1, x1_len), (x2, x2_len), (y1, y_len), _ = self.collate_fn(self.training_queue)
 
+            learning_rate = np.mean(self.learning_queue)
             loss = self._learn_detailed(x1, x1_len, x2, x2_len, y1, y_len, learning_rate)
 
             self.training_queue.clear()
+            self.learning_queue.clear()
 
             return True, loss
         return False, None
