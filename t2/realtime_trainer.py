f7f877413046551091f8ddcafae73d545e035e14
go13 <dima.blinkov@gmail.com>
2023-01-08 18:49:50 +0000

    transformer

--- t2/realtime_trainer.py
+++ t2/realtime_trainer.py
@@ -30,6 +30,19 @@ class RealtimeTrainer(AbstractTrainer):
             return True, loss
         return False, None
 
+    def learn_detailed(self, x1, x2, y):
+        self.training_queue.append((x.split(), y.split()))
+
+        if len(self.training_queue) == self.params.batch_size:
+            (x1, x_len), (y1, y_len), _ = self.collate_fn(self.training_queue)
+
+            loss = self._learn(x1, x_len, y1, y_len)
+
+            self.training_queue.clear()
+
+            return True, loss
+        return False, None
+
     def act(self, xx):
         q = []
         for x in xx:
