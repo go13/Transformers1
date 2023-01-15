3bbf8e96adc25933da7cc50edc12f889926f9a9a
go13 <dima.blinkov@gmail.com>
2023-01-15 23:16:44 +0000

    getting weights of transformer

--- ga_model_runner.py
+++ ga_model_runner.py
@@ -1,6 +1,8 @@
 import time
 import random
-
+import torch
+from collections import OrderedDict
+import torch.nn as nn
 from envs import build_env
 from ga.ga import GA, TargetStringEvaluator, XY, gen_rnd_chars, crossover_string
 from base_model_runners import AbstractModelRunnner
@@ -36,12 +38,45 @@ class NeuralXY(XY):
         self.transformer_pool = transformer_pool
 
     def crossover_transformer(self, xy1, xy2):
-        # xy1_weights = xy1.get_transformer_weights()
-        # xy2_weights = xy2.get_transformer_weights()
+        xy1_weights = xy1.get_transformer_weights()
+        xy2_weights = xy2.get_transformer_weights()
 
         trainer = self.transformer_pool.acquire()
+        new_state = []
 
-        # trainer.get_transformer().load_state_dict(xy2_weights)
+        for k1, _ in xy1_weights.items():
+            shape = xy1_weights[k1].shape
+            v1 = xy1_weights[k1].reshape(-1)
+            v2 = xy2_weights[k1].reshape(-1)
+
+            rnd1 = torch.rand(v1.shape, device="cuda")
+
+            update = (v1 * rnd1 + (1 - rnd1) * v2)
+
+            # mutate
+            ln = len(v1)
+            mutation_rate = 0.01
+            num_of_ones = int(mutation_rate * ln)
+            num_of_zeros = ln - num_of_ones
+            ones_to_mutate = torch.ones(num_of_ones, device="cuda")
+            zeros_to_mutate = torch.zeros(num_of_zeros, device="cuda")
+            to_mutate = torch.cat((ones_to_mutate, zeros_to_mutate), -1).reshape(-1)
+
+            idxs = torch.randperm(ln, device="cuda")
+
+            to_mutate_one_zeros = torch.gather(to_mutate, 0, idxs)
+
+            rnd2 = torch.rand(ln, device="cuda")
+
+            update = rnd2 * to_mutate_one_zeros + (1 - to_mutate_one_zeros) * update
+
+            update = update.reshape(shape)
+            new_state.append((k1, update))
+
+        # pair[0].load_state_dict(OrderedDict(new_state))
+
+
+        trainer.get_transformer().load_state_dict(OrderedDict(new_state))
 
         return trainer
 
