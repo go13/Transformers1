43377de8f87fbf25fe17d294f423f5247e4974ae
go13 <dima.blinkov@gmail.com>
2023-01-22 15:41:08 +0000

    let's test how fast transformer can exchange weights

--- karpathy_weights_ping_pong.py
+++ karpathy_weights_ping_pong.py
@@ -1,68 +1,19 @@
 import torch
 
+from t3_karpathy.gpt_nano_dataloader import GptNanoDataloader
 from t3_karpathy.transformer_config import TransformerConfig
 
 from t3_karpathy.transformer_runner import KarpathyRunner
 
-batch_size = 16  # how many independent sequences will we process in parallel?
-block_size = 32  # what is the maximum context length for predictions?
-max_iters = 50
-eval_interval = 100
-learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embd = 64
-n_head = 4
-n_layer = 4
-dropout = 0.0
-# ------------
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('../input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-# here are all the unique characters that occur in this text
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-# create a mapping from characters to integers
-stoi = {ch: i for i, ch in enumerate(chars)}
-itos = {i: ch for i, ch in enumerate(chars)}
-encode = lambda s: [stoi[c] for c in s]  # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l])  # decoder: take a list of integers, output a string
-
-# Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9 * len(data))  # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
-
-
-# data loading
-def get_batch(split):
-    # generate a small batch of data of inputs x and targets y
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i + block_size] for i in ix])
-    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-
-def get_train_batch():
-    return get_batch('train')
-
-
-def get_val_batch():
-    return get_batch('test')
-
-
 config = TransformerConfig()
 runner = KarpathyRunner(config)
+dataloader = GptNanoDataloader(config)
 
-#let's train for a bit
-runner.train_iterate(100, get_train_batch, get_val_batch)
+runner.train_iterate(50, dataloader.get_train_batch, dataloader.get_val_batch)
 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(runner.generate(context, max_new_tokens=2000)[0].tolist()))
+print(dataloader.decode(runner.generate(context, max_new_tokens=2000)[0].tolist()))
