fc051e98ff5977e1d0c2ff8d9af147889b145822
go13 <dima.blinkov@gmail.com>
2023-02-04 20:44:07 +0000

    ga + karpathy - CompressingTransformerModel

--- t3_karpathy/compressing_playground.py
+++ t3_karpathy/compressing_playground.py
@@ -5,12 +5,11 @@ import torch
 
 config = TransformerConfig(batch_size=128, block_size=32, n_embed=64, n_head=2, n_layer=2)
 dataloader = GptNanoDataloader(config)
-device = 'cuda'
 
 trainer1=CompressingAccumulativeTrainer(config)
 trainer1.train_iterate(5000, dataloader.get_train_batch, dataloader.get_val_batch)
 
-print(dataloader.token_codec.decode(trainer1.generate(torch.zeros((1, 1), dtype=torch.long, device=device), 100)[0].tolist()))
+print(dataloader.token_codec.decode(trainer1.generate(torch.zeros((1, 1), dtype=torch.long, device=config.my_device), 100)[0].tolist()))
 
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
+context = torch.zeros((1, 1), dtype=torch.long, device=config.my_device)
 print(dataloader.token_codec.decode(trainer1.generate(context, max_new_tokens=2000)[0].tolist()))
