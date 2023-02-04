44caf5ff170a674cbf43102bc73768cf79c5285f
go13 <dima.blinkov@gmail.com>
2023-02-04 01:29:54 +0000

    ga + karpathy - CompressingTransformerModel

--- t3_karpathy/compressing_transformer.py
+++ t3_karpathy/compressing_transformer.py
@@ -137,6 +137,23 @@ class CompressingAccumulativeTrainer(AbstractRunner):
     def predict(self, x1):
         return self.predict_list([x1])[0]
 
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -self.config.block_size:]
+            # get the predictions
+            logits = self.runner.forward(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :]  # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+        return idx
+
     def train(self, n=1):
         losses = 0
         for i in range(n):
