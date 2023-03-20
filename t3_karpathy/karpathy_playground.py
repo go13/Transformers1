232e615bfcb112cf70bb88faf52a57f5c1fc93fa
go13 <dima.blinkov@gmail.com>
2023-03-20 14:58:39 +0000

    enhanced karpathy

--- t3_karpathy/karpathy_playground.py
+++ t3_karpathy/karpathy_playground.py
@@ -3,14 +3,14 @@ import torch
 from t3_karpathy.gpt_nano_dataloader import GptNanoDataloader
 from t3_karpathy.transformer_config import TransformerConfig
 
-from t3_karpathy.enhanced_karpathy_transformer import KarpathyRunner
+from t3_karpathy.enhanced_karpathy_transformer import EnhancedKarpathyRunner
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 torch.manual_seed(1337)
 
 config = TransformerConfig(n_embed=64, n_head=4, n_layer=4, batch_size=32) # batch_size=1024, block_size=128, n_embed=64, n_head=4, n_layer=8)
-runner = KarpathyRunner(config)
+runner = EnhancedKarpathyRunner(config)
 dataloader = GptNanoDataloader(config)
 
 runner.train_iterate(25000, dataloader.get_train_batch, dataloader.get_val_batch)
