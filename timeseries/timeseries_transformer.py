bf055de20fc38b525a15012d94831bcaa702dd43
go13 <dima.blinkov@gmail.com>
2023-04-05 03:20:17 +0100

    timeseries fixes

--- timeseries/timeseries_transformer.py
+++ timeseries/timeseries_transformer.py
@@ -124,7 +124,7 @@ class TimeseriesDataloader(object):
         self.codec = TimeseriesCodec()
 
         directory_path = 'US-Stock-Dataset/Data/StockHistory'
-        df, found_files = read_and_merge_csv_files(directory_path, stocks_to_load, start_date='2010-01-01', end_date='2020-12-31')
+        df, found_files = read_and_merge_csv_files(directory_path, stocks_to_load, start_date='2000-01-01', end_date='2020-12-31')
 
         df.drop(columns=['Date'], axis=1, inplace=True)
 
@@ -219,18 +219,22 @@ stocks_to_load = [
 
 dataloader = TimeseriesDataloader(stocks_to_load)
 config = TimeseriesTransformerConfig(
-    batch_size=64,
-    block_size=64,
-    n_embed=32,
+    batch_size=32,
+    block_size=512,
+    n_embed=64,
     n_head=4,
-    n_layer=4,
+    n_layer=8,
     kernel_size=4,
     learning_rate=1e-3,
     channels=dataloader.get_number_of_channels()
 )
-trainer1 = TimeseriesPandasTrainer(config, dataloader=dataloader, model=TimeseriesTransformerModel(config))
+model = TimeseriesTransformerModel(config)
+trainer1 = TimeseriesPandasTrainer(config, dataloader=dataloader, model=model)
 
-trainer1.train_eval(5000)
+trainer1.train_eval(200000)
+
+torch.save(model.state_dict(), "model.pt")
+
 #
 # def step():
 #     start_time = time.time()
